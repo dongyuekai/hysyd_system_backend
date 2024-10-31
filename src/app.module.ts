4eca458dbd3dfcd87cfe4486b3a288cb9bbcd3a3
dongyuekai <457545648@qq.com>
2024-10-31 18:15:02 +0800

    feat: 封装jwt的token相关

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,23 @@ import { Permission } from './user/entities/permission.entity';
 import { RedisModule } from './redis/redis.module';
 import { EmailModule } from './email/email.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    JwtModule.registerAsync({
+      global: true,
+      // configService.get 是读取 .env 配置文件
+      useFactory(configService: ConfigService) {
+        return {
+          secret: configService.get('jwt_secret'),
+          signOptions: {
+            expiresIn: '30m',
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
     // 全局配置文件
     ConfigModule.forRoot({
       isGlobal: true,
