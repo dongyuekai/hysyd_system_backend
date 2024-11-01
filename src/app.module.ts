6fe1217e79e906a155cf2d8d868c15a5fa18d997
dongyuekai <457545648@qq.com>
2024-11-01 16:00:52 +0800

    feat:接口鉴权

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,9 @@ import { RedisModule } from './redis/redis.module';
 import { EmailModule } from './email/email.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { LoginGuard } from './login.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { PermissionGuard } from './permission.guard';
 
 @Module({
   imports: [
@@ -57,6 +60,16 @@ import { JwtModule } from '@nestjs/jwt';
     EmailModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: LoginGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: PermissionGuard,
+    },
+  ],
 })
 export class AppModule {}
